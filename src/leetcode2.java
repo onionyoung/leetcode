543ab60f24ea2bdbc9f88195bce3c50978a84693
onionyoung <lkk50614@yahoo.com.tw>
2016-06-27 21:53:14 +0800

    Add Two Numbers
    You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
    
    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
    Output: 7 -> 0 -> 8

--- src/leetcode2.java
+++ src/leetcode2.java
@@ -4,7 +4,7 @@ public class leetcode2 {
         int val;
         ListNode next;
         ListNode(int x) { val = x; }
-    }
+    } 
 	public static void main(String[] args) {
 		ListNode a = new ListNode(5);
 		//a.next = new ListNode(2);
