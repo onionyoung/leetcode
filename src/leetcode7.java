d7dcbf0022c1bb45fde2f992ed310eae03ed7fcf
onionyoung <lkk50614@yahoo.com.tw>
2016-06-28 20:20:14 +0800

    Reverse Integer
    Reverse digits of an integer.
    
    Example1: x = 123, return 321
    Example2: x = -123, return -321

--- src/leetcode7.java
+++ src/leetcode7.java
@@ -1,4 +1,20 @@
 
 public class leetcode7 {
-
+	public static void main(String[] args) {
+		System.out.println(reverse(0));
+		System.out.println(reverse(-214783648));
+    }
+	public static int reverse(int x) {
+		int i = 0;
+		int sum = 0;
+		if(x == 0)return x;
+		for(;;){
+			if(2147483647 < (long)sum * 10 + x % 10)return 0;
+			if(-2147483647 > (long)sum * 10 + x % 10)return 0;
+			sum = sum * 10 + x % 10;
+			x = x / 10;
+			if(x == 0)break;
+		}
+		return sum;
+    }
 }
